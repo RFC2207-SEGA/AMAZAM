dd0d65d530d95d330406e36a05ced43812c2db42
Alice Blank <alicejblank@gmail.com>
2022-09-07 17:00:07 -0500

    Added server and scripts.

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+// app.use(express.urlencoded({
+//   extended: true,
+// }));
+// app.use(express.json());
+app.use(express.static('./public'))
+
+const PORT = 8080;
+
+app.listen(PORT);
+console.log(`Server listening at http://${PORT}`)
