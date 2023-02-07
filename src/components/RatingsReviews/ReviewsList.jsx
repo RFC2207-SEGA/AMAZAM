7bff681e0b2eefa96d3749819481a0127ccf985a
melissa-gv <melissagilv@gmail.com>
2023-02-07 13:39:09 -0600

    Fixed review photo modal to auto size

--- src/components/RatingsReviews/ReviewsList.jsx
+++ src/components/RatingsReviews/ReviewsList.jsx
@@ -3,7 +3,7 @@ import ReviewTile from './ReviewTile.jsx'
 
 function ReviewsList ({ reviews, reviewsToDiplay }) {
   return (
-    <div className='reviews-list' style={{overflowY: 'scroll'}}>
+    <div className='reviews-list' style={{overflowY: 'auto'}}>
       {reviews.map((review) =>
         <ReviewTile review={review} key={review.review_id} />
       )}
